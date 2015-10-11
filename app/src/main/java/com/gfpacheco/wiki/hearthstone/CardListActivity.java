7c380bd3618eda79c9622b07a46bad631c7d2438
Guilherme Pacheco <guilherme.f.pacheco@gmail.com>
2015-10-10 22:32:14 -0300

    Create project with empty activity

---
+++ app/src/main/java/com/gfpacheco/wiki/hearthstone/CardListActivity.java
@@ -0,0 +1,13 @@
+package com.gfpacheco.wiki.hearthstone;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CardListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_card_list);
+    }
+}
